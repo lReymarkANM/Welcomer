93b1bfbfaa2a369bbd479a4be0027ad30d1dbc1d
ReymarkANM <77568046+lReymarkANM@users.noreply.github.com>
2022-04-04 13:11:34 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.Token = 'OTM1ODcxNDY2MjQ1ODQ5MDg4.YfE8OQ._4P2Y7sZVkIeMUFsKq9RRfygWJQ'; //Bot Token
+exports.WelChan = '932842773340639262'; //Channel ID where welcome msgs should go
+exports.LeaveChan = `932841013565874216`; //Channel ID where leave msgs should go
+exports.WelMsg = ``; //Msg sent for the user joining.. use the "[user]" tag to mention the user \\ Default - "[user] joined the server!"
+exports.WelImg = ``; //Welcome Image Background Link \\ Default - A nice pic taken from reddit
+exports.LeaveImg = ``; //Leave Image Bakcground Link \\ Default - A nice pic taken from reddit
+exports.LeaveMsg = ``; //Msg sent for the user leaving.. use the "[user]" tag to enter user's tag \\ Default - "[user] left the server :("
